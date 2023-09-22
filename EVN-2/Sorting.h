fab830de4bc591097f911de1cd6d66689334b518
roza-galstyan <144101518+roza-galstyan@users.noreply.github.com>
2023-09-22 18:47:46 +0400

    Update Sorting.h

--- EVN-2/Sorting.h
+++ EVN-2/Sorting.h
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <vector>
 
+
 void Sorting_complex_numbers (std::vector<Complex_number> &arr){
     Complex_number temp;
     for (int i=0;i+1<arr.size();++i){
          for (int j=i+1;j<arr.size();++j){
-            if (arr[i].a>arr[j].a){
+            if (arr[i].Absolute_value()>arr[j].Absolute_value()){
                 temp.a=arr[j].a;
                 temp.b=arr[j].b;
                 arr[j].a=arr[i].a;
@@ -13,16 +14,8 @@ void Sorting_complex_numbers (std::vector<Complex_number> &arr){
                 arr[i].a=temp.a;
                 arr[i].b=temp.b;
             };
-            if (arr[i].a==arr[j].a){
-                if (arr[i].b>arr[j].b){
-                    temp.a=arr[j].a;
-                temp.b=arr[j].b;
-                arr[j].a=arr[i].a;
-                arr[j].b=arr[i].b;
-                arr[i].a=temp.a;
-                arr[i].b=temp.b;
+            
                 }
-            }
-                }
+
                     } 
                         };
